fbc86daf3de50e970d6ed1ffecd466fd958225f1
zoécostan <zoe.costan@orange.fr>
2023-06-30 10:17:44 +0200

    clean appflask sur pc perso

--- AppFlask/appflask.py
+++ AppFlask/appflask.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify, request
 import requests
 from flask_sqlalchemy import SQLAlchemy
-import json
 import math
 import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-# from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense
 
 app = Flask(__name__)
 
+# Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zoecostan:565399@localhost:3306/meetdrone4'
 
 db = SQLAlchemy(app)
@@ -31,93 +29,16 @@ class Airport(db.Model):
     numAirports = db.Column(db.Integer)
     city = db.Column(db.Enum('true', 'false'))
 
-# Load the trained tf model
-model = tf.keras.models.load_model('model.h5')
-
 # Basic request without parameters
 @app.route('/')
 def hello():
-    return 'Add parameters to the request (lon, lat)'
+    return 'Add parameters to the request.'
 
-# Meteo forecast only
-@app.route('/forecast/<lat>/<lng>')
-def process_forecast(lat, lng):
-    api_key = 'b1723b95bdb54629c30ac93f1bfe77c2'
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&units=metric&appid={api_key}'
-
-    response = requests.get(url)
-    forecast = response.json()
-    weather_description = forecast["weather"][0]["description"]
-
-    # Assumer une valeur de 1 pour "rain" dans weather_description, sinon 0
-    input_data = [0, 0, 0, 0]
-
-    # Récupérer les paramètres avec des valeurs par défaut de None
-    temperature = request.args.get('temperature')
-    humidity = request.args.get('humidity')
-    wind_speed = request.args.get('wind_speed')
-    cloudiness = request.args.get('cloudiness')
-
-    # Convertir les valeurs en float si elles ne sont pas None
-    if temperature is not None:
-        input_data[0] = float(temperature)
-    if humidity is not None:
-        input_data[1] = float(humidity)
-    if wind_speed is not None:
-        input_data[2] = float(wind_speed)
-    if cloudiness is not None:
-        input_data[3] = float(cloudiness)
-
-    # Créer un tableau NumPy avec les valeurs d'entrée
-    input_data = np.array(input_data, dtype=np.float32)
-
-    # Assurez-vous que les données d'entrée ont la bonne forme
-    input_data = np.expand_dims(input_data, axis=0)
-
-    # Prédiction du modèle
-    prediction = model.predict(input_data)[0]
-
-    if prediction >= 0.5:
-        result = "El drone debe aterrizar."
-    else:
-        result = "El drone puede seguir volando."
-
-    return jsonify({'result': forecast, 'decision': result})
-
-#La fonction process_forecast est définie en tant que point de terminaison (endpoint) de votre application Flask avec la route /forecast/<lat>/<lng>.
-#Lorsqu'une requête est effectuée à cette route avec des valeurs de latitude et de longitude spécifiées, par exemple /forecast/48.8566/2.3522, Flask récupère ces valeurs et les passe en tant qu'arguments à la fonction process_forecast.
-#À l'intérieur de la fonction process_forecast, les valeurs de latitude et de longitude sont utilisées pour construire l'URL de l'API OpenWeatherMap.
-#Ensuite, une requête GET est effectuée à l'URL construite en utilisant requests.get(url).
-#La réponse de l'API est obtenue en appelant .json() sur l'objet de réponse, ce qui renvoie les données de prévision au format JSON.
-#Enfin, les données de prévision sont renvoyées en tant que réponse JSON à l'appelant de la requête.
-
-# Geofence only
-@app.route('/check_airport/<latitude>/<longitude>', methods=['GET'])
-def check_airport(latitude, longitude):
-    latitude = float(latitude)
-    longitude = float(longitude)
-
-    # Interroger la base de données pour trouver les aéroports
-    airports = Airport.query.all()
-
-    response = {}  # Initialiser la variable de réponse
-
-    # Vérifier la distance entre chaque aéroport et les coordonnées fournies
-    for airport in airports:
-        airport_latitude = float(airport.lat)
-        airport_longitude = float(airport.lon)
-        distance = haversineGreatCircleDistance(latitude, longitude, airport_latitude, airport_longitude)
-        
-        if distance <= 8000:  # Vérifier si la distance est inférieure ou égale à 8000 mètres (8 km)
-            response['message'] = "Veuillez vous éloigner de l'aéroport."
-            break
-    else:
-        response['message'] = "Vous êtes à une distance sécuritaire de l'aéroport."
-
-
-    return jsonify(response)
+# Load the trained tf model
+model = tf.keras.models.load_model('model.h5')
 
 # All previsions (geofence and meteo including tensorflow)
+# http://localhost:5000/allinfo/<lat>/<long>
 @app.route('/allinfo/<latitude>/<longitude>', methods=['GET'])
 def all_info(latitude, longitude):
     latitude = float(latitude)
@@ -194,6 +115,7 @@ def all_info(latitude, longitude):
 
     return jsonify(result)
    
+# Annexe function   
 def haversineGreatCircleDistance(latitudeFrom, longitudeFrom, latitudeTo, longitudeTo):
     # convertir de degrés à radians
     earthRadius = 6371000
