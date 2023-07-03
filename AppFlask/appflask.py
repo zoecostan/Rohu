5f00adf427660b500df53ffaf2cd9bb06084d179
Zoe Costan <zoe.costan@orange.fr>
2023-07-03 16:58:13 +0200

    problemes auth

--- AppFlask/appflask.py
+++ AppFlask/appflask.py
@@ -7,15 +7,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+from functools import wraps
 
 app = Flask(__name__)
 
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "Rohu2023"  # Change this!
-jwt = JWTManager(app)
-
+# Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zoecostan:565399@localhost:3306/meetdrone4'
+
+# Flask JWT Extended config
+app.config['JWT_SECRET_KEY'] = 'Rohu2023'  # secret key
+jwt = JWTManager(app)
 
 db = SQLAlchemy(app)
 
@@ -34,55 +39,61 @@ class Airport(db.Model):
     numAirports = db.Column(db.Integer)
     city = db.Column(db.Enum('true', 'false'))
 
-# Load the trained tf model
-model = tf.keras.models.load_model('model.h5')
-
-# Annexe function
-def haversineGreatCircleDistance(latitudeFrom, longitudeFrom, latitudeTo, longitudeTo):
-    # convert degrés to radians
-    earthRadius = 6371000
-    latFrom = math.radians(latitudeFrom)
-    lonFrom = math.radians(longitudeFrom)
-    latTo = math.radians(latitudeTo)
-    lonTo = math.radians(longitudeTo)
-
-    latDelta = latTo - latFrom
-    lonDelta = lonTo - lonFrom
-
-    angle = 2 * math.asin(math.sqrt(math.pow(math.sin(latDelta / 2), 2) +
-                     math.cos(latFrom) * math.cos(latTo) * math.pow(math.sin(lonDelta / 2), 2)))
-    distance = angle * earthRadius
-
-    return round(distance, 2)
-
-# Route pour l'authentification
-@app.route('/auth', methods=['POST'])
-def authenticate():
-    username = request.json.get('username')
-    password = request.json.get('password')
-
-    # Vérification des informations d'identification
-    if username == "zoecostan" and password == "565399":
-        # Génération du jeton JWT
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token), 200
-    else:
-        return jsonify(message="Invalid username or password"), 401
-
-# ...
-
 # Basic request without parameters
 @app.route('/')
 def hello():
-    return 'Add parameters to the request (lon, lat)'
+    return 'Add parameters to the request.'
 
-# All previsions (geofence and meteo including tensorflow)
-# http://localhost:5000/allinfo/lat/long
+# Load the trained tf model
+model = tf.keras.models.load_model('model.h5')
+
+# Fonction de vérification des autorisations
+def check_jwt_authorization(latitude, longitude):
+    if latitude and longitude:
+        # Vérifier les autorisations ici
+        return True
+    return False
+
+# Décorateur personnalisé pour gérer l'authentification JWT
+def jwt_latitude_longitude_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        latitude = request.view_args.get('latitude')
+        longitude = request.view_args.get('longitude')
+
+        # Vérifier l'authentification JWT et les autorisations
+        if not jwt_required():
+            return jsonify(message='Authentification JWT requise'), 401
+        if not check_jwt_authorization(latitude, longitude):
+            return jsonify(message='Autorisation requise'), 403
+
+        # Exécuter la fonction avec les paramètres
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+# Protected route - All previsions (geofence and meteo including tensorflow)
+# http://localhost:5000/allinfo/<latitude>/<longitude>
 @app.route('/allinfo/<latitude>/<longitude>', methods=['GET'])
-@jwt_required
+@jwt_required()
 def all_info(latitude, longitude):
+    current_user = get_jwt_identity()
     latitude = float(latitude)
     longitude = float(longitude)
+
+    # Vérifiez si le jeton d'accès est présent et valide
+    access_token = request.headers.get('Authorization')
+    if access_token is None:
+        return jsonify(message='Authentification JWT requise'), 401
+
+    # Inclure le jeton d'accès dans les en-têtes de la requête
+    headers = {
+        'Authorization': access_token
+    }
+
+    # Effectuez votre requête à la route protégée en incluant le jeton d'accès
+    url = f'http://localhost:5000/allinfo/{latitude}/{longitude}'
+    response = requests.get(url, headers=headers)
 
     api_key = 'b1723b95bdb54629c30ac93f1bfe77c2'
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={api_key}'
@@ -154,6 +165,40 @@ def all_info(latitude, longitude):
     }
 
     return jsonify(result)
+
+# Route pour l'authentification et la génération du jeton d'accès
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Vérifier les informations d'identification de l'utilisateur (exemple simple)
+    if username == 'zoecostan' and password == '565399':
+        # Générer le jeton d'accès pour l'utilisateur authentifié
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message='Identifiants invalides'), 401
+   
+# Annexe function   
+def haversineGreatCircleDistance(latitudeFrom, longitudeFrom, latitudeTo, longitudeTo):
+    # convertir de degrés à radians
+    earthRadius = 6371000
+    latFrom = math.radians(latitudeFrom)
+    lonFrom = math.radians(longitudeFrom)
+    latTo = math.radians(latitudeTo)
+    lonTo = math.radians(longitudeTo)
+
+    latDelta = latTo - latFrom
+    lonDelta = lonTo - lonFrom
+
+    angle = 2 * math.asin(math.sqrt(math.pow(math.sin(latDelta / 2), 2) +
+                     math.cos(latFrom) * math.cos(latTo) * math.pow(math.sin(lonDelta / 2), 2)))
+    distance = angle * earthRadius
+
+    return round(distance, 2)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
